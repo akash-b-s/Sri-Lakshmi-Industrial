56b912699697c5e331b13ef4f603304f6d2fd70f
Akash B S <akash190504@gmail.com>
2025-02-22 19:29:15 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom'; 
 import './navbar.css';
-import logo from '../assets/logo1.png'; 
+import logo from '../assets/logo2.png'; 
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false); 
